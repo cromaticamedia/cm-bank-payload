8ab0a879bc461b38ee3258af3ff3855e0f689a2d
sadpisco <leo99153@gmail.com>
2026-05-05 17:45:53 -0500

    Build fixed

--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -1,10 +1,10 @@
-import NavigationBar from '@/components/molecules/NavigationBar'
-import Footer from '@/components/molecules/Footer'
+import NavigationBar from '@/components/molecules/NavigationBar/NavigationBar'
+import Footer from '@/components/molecules/Footer/Footer'
 import type { LocaleCode } from '@/config/locales'
 
 type Props = {
   children: React.ReactNode
-  params: Promise<{ locale: LocaleCode }>
+  params: Promise<{ locale: string }>
 }
 
 export default async function LocaleLayout({ children, params }: Props) {
@@ -12,9 +12,9 @@ export default async function LocaleLayout({ children, params }: Props) {
 
   return (
     <>
-      <NavigationBar locale={locale} />
+      <NavigationBar locale={locale as LocaleCode} />
       {children}
-      <Footer locale={locale} />
+      <Footer locale={locale as LocaleCode} />
     </>
   )
 }
