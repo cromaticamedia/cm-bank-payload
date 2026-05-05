49d4e98f97da536f6702ca5f88a033c2de5cd550
sadpisco <leo99153@gmail.com>
2026-05-04 19:59:03 -0500

    Clerk integration implemented

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { cn } from '@/utils/styles'
 import { MiChroma, Microgramma, SpaceGrotesk } from '@/fonts'
 import './styles.css'
@@ -17,8 +18,26 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
       className={cn(MiChroma.variable, Microgramma.variable, SpaceGrotesk.variable)}
       suppressHydrationWarning
     >
-      <body>
+      {/* <body>
         <main>{children}</main>
+      </body> */}
+      <body>
+        <ClerkProvider>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+        </ClerkProvider>
       </body>
     </html>
   )
